41753c51bcf0b6a960aa06d91ddf45b0f96b30ac
opilane <opilane@HP-CZC6297B8P>
2022-03-16 12:18:10 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Koolilused.Startup))]
+namespace Koolilused
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
