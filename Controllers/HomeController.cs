383bf3aa7d5b076d1dce4a2a9ff5e53ef7b8d050
opilane <opilane@HP-CZC6297B8P>
2022-03-23 12:26:35 +0200

    fvc

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Koolilused.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,16 +17,75 @@ namespace Koolilused.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Meie Koolitus on uuenduslik ja tööturu vajadustele suunatud elukestva õppe keskus. ";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Kontakti lehe";
 
             return View();
+        }
+    KoolitusContext db = new KoolitusContext();
+        public ActionResult Opitajad()
+        {
+            IEnumerable<Opetaja> Opitajad = db.Opetajas;
+            return View(Opitajad);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Opetaja op)
+        {
+            db.Opetajas.Add(op);
+            db.SaveChanges();
+            return RedirectToAction("Opitajad");
+
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Opetaja g = db.Opetajas.Find(id);
+            if (g == null)
+            {
+                return HttpNotFound();
+            }
+            return View(g);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Opetaja g = db.Opetajas.Find(id);
+            if (g == null)
+            {
+                return HttpNotFound();
+            }
+            db.Opetajas.Remove(g);
+            db.SaveChanges();
+            return RedirectToAction("Opitajad");
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            Opetaja g = db.Opetajas.Find(id);
+            if (g == null)
+            {
+                return HttpNotFound();
+            }
+            return View(g);
+        }
+        [HttpPost, ActionName("Edit")]
+        public ActionResult EditConfirmed(Opetaja op)
+        {
+            db.Entry(op).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Opitajad");
         }
     }
 }
--- Models/Opetaja.cs
+++ Models/Opetaja.cs
@@ -10,6 +10,6 @@ namespace Koolilused.Models
         public int Id { get; set; }
         public string OpetajaEesnimi { get; set; }
         public string OpetajaPerenimi { get; set; }
-        public int Telefon { get; set; }
+        public string Telefon { get; set; }
     }
 }
